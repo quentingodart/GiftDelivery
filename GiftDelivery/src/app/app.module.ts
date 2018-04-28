5d36e4bd435062a58d8f36c05f5b9b3d9fac05e1
Quentin Godart <quentin.godart@epitech.eu>
2018-04-28 18:49:42 -0400

    architecture projet

--- GiftDelivery/src/app/app.module.ts
+++ GiftDelivery/src/app/app.module.ts
@@ -1,20 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutComponent } from './dashboard/layout/layout.component';
+import { HeaderComponent } from './dashboard/layout/header/header.component';
+import { FooterComponent } from './dashboard/layout/footer/footer.component';
 
+
+const appRoutes : Routes = [
+  {
+    path: '',
+    redirectTo : '/accueil',
+    pathMatch : 'full'
+  },
+  {
+    path: 'accueil',
+    component: DashboardComponent,
+    data: { title: 'Accueil' }
+  },
+  {
+    path: 'connection',
+    component: LoginComponent,
+    data: { title: 'Connection' }
+  },
+  {
+    path: 'inscription',
+    component: SigninComponent,
+    data: { title: 'Inscription' }
+  },
+  { path: '**', component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SigninComponent
+    SigninComponent,
+    DashboardComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true } // <-- debugging purposes only
+  ),
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
