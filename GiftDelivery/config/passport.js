b86a4a5d3f835d4ad46116a944de0dd04cf2db23
foncel_a <anais.foncel@epitech.eu>
2018-04-29 13:58:00 +0200

    Correction passport.js

--- GiftDelivery/config/passport.js
+++ GiftDelivery/config/passport.js
@@ -2,7 +2,7 @@ var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // load up the user model
-var User = require('../models/user');
+var User = require('../models/Utilisateur');
 var config = require('../config/database'); // get db config file
 
 module.exports = function(passport) {
@@ -10,7 +10,7 @@ module.exports = function(passport) {
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
   opts.secretOrKey = config.secret;
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.id}, function(err, user) {
+    Utilisateur.findOne({id: jwt_payload.id}, function(err, user) {
           if (err) {
               return done(err, false);
           }
