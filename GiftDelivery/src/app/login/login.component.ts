5f28449cfda3cec1202f8e998fc62af1f77c3985
Quentin Godart <quentin.godart@epitech.eu>
2018-04-29 04:45:13 -0400

    logique inscription + connexion + correction bug css

--- GiftDelivery/src/app/login/login.component.ts
+++ GiftDelivery/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private loginUsername : string = "";
+  private loginPassword : string = "";
+
+  private message : string = "";
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  login() {
+    console.log("loginUsername : [" + this.loginUsername + "] + loginPassword : [" + this.loginPassword + "]");
+    if (this.loginUsername != "" && this.loginPassword != "") {
+      this.message = null;
+      this.router.navigate(["/accueil"]);
+    }
+    else {
+      this.message = "Veuillez renseigner les champs.";
+    }
+  }
 }
--- GiftDelivery/src/app/signin/signin.component.ts
+++ GiftDelivery/src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  private lastname : string = "";
+  private firstname : string = "";
+  private username : string = "";
+  private email : string = "";
+  private password : string = "";
+  private confirmPassword : string = "";
+
+  private message : string = "";
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  signin() {
+    if (this.lastname != "" && this.firstname != "" && this.username != ""
+        && this.email != "" && this.password != "" && this.confirmPassword != "") {
+          this.message = null;
+          if (this.password == this.confirmPassword) {
+            console.log("lastname : " + this.lastname + " firstname : " + this.firstname + " username : " + this.username + " email : " +
+            this.email + " password : " + this.password + " confirmPassword : " + this.confirmPassword);
+            this.message = null;
+            this.router.navigate(["/accueil"]);
+          }
+          else {
+            this.message = "Les deux mots de passe doivent être identiques.";
+          }
+    }
+    else {
+      this.message = "Vous devez renseigner tous les champs.";
+    }
   }
 
 }
