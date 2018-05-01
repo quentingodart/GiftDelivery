e32fbfcc8e0a76fc65bbf3ee9424e0ba2be8ff25
Quentin Godart <quentin.godart@epitech.eu>
2018-05-01 02:47:55 -0400

    profil utilisateur

--- GiftDelivery/src/app/dashboard/profil/profil.component.ts
+++ GiftDelivery/src/app/dashboard/profil/profil.component.ts
@@ -15,7 +15,7 @@ export class ProfilComponent implements OnInit {
   private userToken : string = "";
   private username : string = "";
 
-  private user : any;
+  private user : any = null;
 
   constructor(private http: HttpClient, private router: Router) {
 
@@ -25,18 +25,6 @@ export class ProfilComponent implements OnInit {
    }
 
   ngOnInit() {
-<<<<<<< HEAD
-  }
-
-private Data = {username:''};
-  getUser() {
-    this.http.get('/api/me', {params: {username : this.username}}).subscribe(resp => {
-    this.user = resp;
-    console.log("Récuprération de l'utilisateur : " + this.user);
-    console.log("User1 : " + this.user.username);
-  }, err => {
-    console.log("Récupération profil erreur");
-=======
   this.http.get('/api/me', {params: {username:this.username}}).subscribe(data => {
     this.user = data;
     console.log(this.user);
@@ -44,7 +32,6 @@ private Data = {username:''};
     if(err.status === 401) {
       console.log("Récupération profil erreur");
     }
->>>>>>> d2e9a2a32c466966aca73a0db2d33b15c92116be
   });
   }
 }
